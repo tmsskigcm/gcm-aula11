625e50679d52a27fbe6998be949f92b74d4f9dca
1700475 <1700475@sala31218.fit.net>
2017-06-21 22:12:09 -0300

    Implementação do método sacar. Resolve #3

--- src/main/java/edu/impacta/gcmaula11/ContaBancaria.java
+++ src/main/java/edu/impacta/gcmaula11/ContaBancaria.java
@@ -14,8 +14,13 @@ public class ContaBancaria {
 		// FALTA IMPLEMENTAR
 		// Se o valor for menor ou igual ao saldo, deve retirar o valor do saldo e retornar true
 		// Se o valor ultrapassar o saldo, deve retornar false
-		return true;
-	}
+		if (valor <= saldo) {
+			saldo -= valor;
+			return true;
+			}
+			return false;
+			}
+	
 	
 	public long getSaldo() {
 		// FALTA IMPLEMENTAR
